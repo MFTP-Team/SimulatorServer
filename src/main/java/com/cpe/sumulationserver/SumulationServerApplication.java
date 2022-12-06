a19d6ac034b15b73b06a2f3270866c56796f62a0
Martin <martin.jourjon42@gmail.com>
2022-12-06 17:23:47 +0100

    feat: init project

---
+++ src/main/java/com/cpe/sumulationserver/SumulationServerApplication.java
@@ -0,0 +1,13 @@
+package com.cpe.sumulationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumulationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumulationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpe/sumulationserver/SumulationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpe.sumulationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumulationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
